c976f11c7d5eddb2fe2ad82c3e7dcac7c3d54898
Edson A. Sensato <esensato@hotmail.com>
2021-04-06 16:40:10 -0300

    Update webpack.config.js

--- react-setup/webpack.config.js
+++ react-setup/webpack.config.js
@@ -1,13 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path"); 
 
 module.exports = { entry: "./index.js", 
-    output: { path: path.join(__dirname, "dist", "assets"), 
+    output: { path: path.join(__dirname, "dist"), 
               filename: "bundle.js" 
     },
     module: {
         rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
     },
-    plugins: [new HtmlWebpackPlugin({template:"index.html", title:"Setup React"})]
+    plugins: [new HtmlWebpackPlugin({template:"index.html", title:"Setup React"}),
+              new CopyPlugin({ patterns: [ { from: "img", to: "img" } ] })]
 
 };
